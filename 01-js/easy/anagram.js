252d3adb9f80f90c4f4ef83b99f3309b7ae09a22
kash1ish <kash1ish2114@gmail.com>
2025-05-14 20:45:35 +0530

    Solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
 
+  str1.replace(/\s+/g,'');
+  str2.replace(/\s+/g,'');
+  
+  let arr1 = str1.split('').sort().join('');
+  let arr2 = str2.split('').sort().join('');
+
+  if(arr1 == arr2){
+    return true;
+  }
+  return false;
 }
 
 module.exports = isAnagram;
